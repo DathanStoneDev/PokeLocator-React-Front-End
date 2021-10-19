214cf39bcb9336db7d7aa4e2ea87e290e0629fbf
Dathan Stone <developer.stone@outlook.com>
2021-10-19 09:49:04 -0700

    Removed the PokemonLocations.js file as it was not needed. Updated the App.js file to hold the const variables for pagination, urls and data. Thes These are passed in as props in the return statement. Updated the Navbar.js file to recieve the handleUrl prop and have all a tags use the prop to set the specific url whurl when clicked. That url will then be passed into the useList custom Hook and Pokemon will display.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Navbar from "./Navbar";
-import PokemonLocations from "./PokemonLocations";
+import PaginationBar from "./PaginationBar";
+import CardList from "./CardList";
+import { useState } from "react";
+import useList from "./useListCustomHook";
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pokemonPerPage] = useState(50);
+  const [url, setUrl] = useState("");
+  const handleUrl = (url)=> setUrl(url);
+  const {data: cards, totalPokemon: pokemon} = useList(url + "?page=" + currentPage + "&size="+ pokemonPerPage);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  //"http://localhost:8080/api/pokemon/ultra-sun-and-moon/list?page="
+
+
+  
+
   return (
     <div className="container">
-      <Navbar />
-      <PokemonLocations />
-
+      <Navbar handleUrl={handleUrl}/>
+      <div className ="poke-locations">
+            <div className="pagination-bar">
+                <PaginationBar pokemonPerPage={pokemonPerPage} totalPokemon={pokemon} paginate={paginate}/>
+            </div>
+            <CardList cards={cards}/>
+        </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,35 @@
-const Navbar = () => {
+
+
+const Navbar = ({handleUrl}) => {
+
+  const sunAndMoon = 'http://localhost:8080/api/pokemon/ultra-sun-and-moon/list';
+  const xAndY = '';
+  const swordAndShield = '';
+  const letsGo = '';
+
   return (
     <nav id="navbar">
       <ul className="game-list">
-        <li className="game-item">Pokemon X and Y</li>
-        <li className="game-item">Pokemon Ultra Sun and Moon</li>
-        <li className="game-item">Pokemon Sword and Shield</li>
-        <li className="game-item">Let's Go Eevve and Let's Go Pikachu</li>
+        <li className="game-item">
+          <a onClick={() => handleUrl(xAndY)} href='!#'>
+            Pokemon X and Y
+          </a>
+        </li>
+        <li className="game-item">
+          <a onClick={() => handleUrl(sunAndMoon)} href='!#'>
+            Pokemon Ultra Sun and Moon
+          </a>
+        </li>
+        <li className="game-item">
+          <a onClick={() => handleUrl(swordAndShield)} href='!#'>
+            Pokemon Sword and Shield
+          </a>
+        </li>
+        <li className="game-item">
+          <a onClick={() => handleUrl(letsGo)} href='!#'>
+            Let's Go Eevve and Let's Go Pikachu
+          </a>
+        </li>
       </ul>
     </nav>
   );
